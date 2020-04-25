fd60acf1ede86c2b3692729b2746df32eabcaa7d
zizo2310 <Moataz_fawzy@live.com>
2020-04-25 18:43:35 -0400

    text mesh

---
+++ Assets/Moataz Script/Scripts/Rotate2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate2 : MonoBehaviour
+{
+public float speed = 75f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right, speed * Time.deltaTime);
+    }
+}
